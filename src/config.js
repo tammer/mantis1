b75fc668ec042ce929c8f5a7232948bd7b8eaebd
Tammer Kamel <tammer@tammer.com>
2026-02-21 06:19:18 -0500

    change sidebar width

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // ==============================|| THEME CONSTANT ||============================== //
 
 export const APP_DEFAULT_PATH = '/dashboard/default';
-export const DRAWER_WIDTH = 260;
+export const DRAWER_WIDTH = 360;
 export const MINI_DRAWER_WIDTH = 60;
 
 
