90d80817a9815de94721613bdd68de3fac4f9fb6
Tammer Kamel <tammer@tammer.com>
2026-02-21 10:52:34 -0500

    .

--- src/layout/Dashboard/Header/HeaderContent/index.jsx
+++ src/layout/Dashboard/Header/HeaderContent/index.jsx
@@ -3,7 +3,6 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import Box from '@mui/material/Box';
 
 // project imports
-import Search from './Search';
 import Profile from './Profile';
 import Notification from './Notification';
 import MobileSection from './MobileSection';
@@ -11,16 +10,14 @@ import MobileSection from './MobileSection';
 // ==============================|| HEADER - CONTENT ||============================== //
 
 export default function HeaderContent() {
-  const downLG = useMediaQuery((theme) => theme.breakpoints.down('lg'));
+  const downMD = useMediaQuery((theme) => theme.breakpoints.down('md'));
 
   return (
     <>
-      {!downLG && <Search />}
-      {downLG && <Box sx={{ width: '100%', ml: 1 }} />}
-
+      <Box sx={{ flexGrow: 1 }} />
       <Notification />
-      {!downLG && <Profile />}
-      {downLG && <MobileSection />}
+      {!downMD && <Profile />}
+      {downMD && <MobileSection />}
     </>
   );
 }
