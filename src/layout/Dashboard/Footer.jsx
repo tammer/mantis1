c35f103541d1901a37560ca437ce8605cd386611
Tammer Kamel <tammer@tammer.com>
2026-02-21 16:50:09 -0500

    .

--- src/layout/Dashboard/Footer.jsx
+++ src/layout/Dashboard/Footer.jsx
@@ -9,13 +9,13 @@ export default function Footer() {
       sx={{ gap: 1.5, alignItems: 'center', justifyContent: 'flex-end', p: '24px 16px 0px', mt: 'auto' }}
     >
       <Stack direction="row" sx={{ gap: 1.5, alignItems: 'center', justifyContent: 'space-between' }}>
-        <Link href="https://codedthemes.com/about-us/" target="_blank" variant="caption" color="text.primary">
+        <Link href="#" variant="caption" color="text.primary">
           About us
         </Link>
-        <Link href="https://mui.com/legal/privacy/" target="_blank" variant="caption" color="text.primary">
+        <Link href="#" variant="caption" color="text.primary">
           Privacy
         </Link>
-        <Link href="https://mui.com/store/terms/" target="_blank" variant="caption" color="text.primary">
+        <Link href="#" variant="caption" color="text.primary">
           Terms
         </Link>
       </Stack>
