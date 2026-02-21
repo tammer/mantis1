069eab9e091c9dd8e5db30634c0509c735642266
Tammer Kamel <tammer@tammer.com>
2026-02-21 07:36:48 -0500

    .

--- src/pages/component-overview/example1.jsx
+++ src/pages/component-overview/example1.jsx
@@ -13,10 +13,19 @@ export default function Example1Page() {
     <Grid container spacing={3}>
       <Grid size={{ xs: 12, lg: 6 }}>
         <Stack sx={{ gap: 3 }}>
-          <MainCard title="Card 1">
-            <Typography variant="body1">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Typography>
+          <MainCard title="February 12, 2026">
+            <Stack sx={{ gap: 1.5 }}>
+              <Typography variant="h2">The Story Behind Haystack's Investment in Blockit</Typography>
+              <Typography variant="h5">Blockit launches with $5M seed financing, building the world's first calendar network.</Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </Typography>
+            </Stack>
           </MainCard>
         </Stack>
       </Grid>
